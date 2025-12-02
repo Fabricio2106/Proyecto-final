5473e24bb62d636f1007e023b96c128a85bc2327
Vanessa Susaya <susayavanessa9@gmail.com>
2025-12-02 14:41:40 -0500

    modificar repositorio de ordenes

--- src/main/java/com/techsolution/gestion_app/repository/OrderRepository.java
+++ src/main/java/com/techsolution/gestion_app/repository/OrderRepository.java
@@ -1,5 +1,7 @@
 package com.techsolution.gestion_app.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.techsolution.gestion_app.domain.entities.Order;
+import java.util.List;
 public interface OrderRepository extends JpaRepository<Order, Long> {
+    List<Order> findByCustomerId(Long customerId);
 }
